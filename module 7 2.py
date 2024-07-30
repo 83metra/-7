624ba45d8b3566710a10336571f75805bd31cb0f
83metra <jumo_004@mail.ru>
2024-07-30 22:44:29 +0200

    Update module 7 2.py
    Убран лишний код.

--- module 7 2.py
+++ module 7 2.py
@@ -1,13 +1,11 @@
 def custom_write(file_name, items):
-    anfang = 0 # позиция курсора перед записью
-    str_count = 0 # счётчик записанных строк
-    strings_positions = {} # словарь {(<номер строки>, <байт начала строки>) : записываемая строка}
+    str_count = 0                                # счётчик записанных строк
+    strings_positions = {}                       # словарь {(<номер строки>, <байт начала строки>) : записываемая строка}
     file = open(str(file_name), 'r+', encoding= 'utf-8')
     for string in items:
-        anfang = file.tell() # позиция курсора до записи
-        file.write(f'{string}\n')
         str_count += 1
-        strings_positions.update({(str_count, anfang): string})
+        strings_positions.update({(str_count, file.tell()): string})
+        file.write(f'{string}\n')
     file.close()
     return strings_positions
 
